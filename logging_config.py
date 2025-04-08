b7951fa9a87f3d1ab30ec38cc879b8273d95b40e
svplaksin <pl.serg91@gmail.com>
2025-04-08 19:03:19 +0300

    Add logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,17 @@
+import os
+import logging
+
+
+def setup_logging(log_file='logs/app_log', log_level=logging.INFO):
+    log_dir = os.path.dirname(log_file)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logging.basicConfig(
+        filename=log_file,
+        level=log_level,
+        format='%(asctime)s - %(name)s - %(module)s - %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S',
+    )
+    logger = logging.getLogger(__name__)
+    return logger
