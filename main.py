80a209f753eb51a217a13336f6a655b82cf3dde8
svplaksin <pl.serg91@gmail.com>
2025-04-09 11:34:27 +0300

    Move functions from main.py into utils.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import json
 import os
-from dotenv import load_dotenv
-import pandas as pd
-import requests
+import signal
+import sys
 import time
-from logging_config import setup_logging
-from api_client import get_debt_amount
 
+import pandas as pd
+from dotenv import load_dotenv
+
+from api_client import get_debt_amount
+from logging_config import setup_logging
+from utils import save_dataframe_to_excel, save_temp_data, signal_handler
 
 # Set up logging
 logger = setup_logging()
@@ -19,11 +21,19 @@ if not API_TOKEN:
     logger.error('API_TOKEN is not found')
     exit()
 
-# Define the temporary files directory (relative to the current directory)
-TEMP_FILES_DIR = "temp_files"
+# Constants
+TEMP_FILES_DIR = "temp_files" # # Define the temporary files directory (relative to the current directory)
+FINAL_FILE = 'numbers_with_debt.xlsx' # Name for the final file
+SAVE_INTERVAL = 20
 
 # Make sure the temporary files directory exists
 os.makedirs(TEMP_FILES_DIR, exist_ok=True)
+
+# Обработчик сигнала
+signal.signal(signal.SIGINT,
+              lambda sig,
+                frame: signal_handler(sig, frame, temp_data, counter, logger, TEMP_FILES_DIR)
+              )
 
 #Load the Excel file
 try:
@@ -31,63 +41,55 @@ try:
     logger.info(f'File loaded successfully. Found {len(df)} numbers')
 except FileNotFoundError:
     logger.error('Error: Numbers file not found')
-    exit()
+    sys.exit(1)
 except Exception as e:
     logger.exception(f'Error reading Excel file: {e}')
-    exit()
+    sys.exit(1)
 
 # Ensure the 'Debt Amount' column exists
 if 'Debt Amount' not in df.columns:
     df['Debt Amount'] = None  # Initialize with None
     logger.info("Created new column 'Debt Amount'")
 
-# Set the interval for saving DataFrame
-SAVE_INTERVAL = 5 # Save every 20 requests
+# Main processing
 counter = 0
-
-# Prepare a list to store the rows for temporary saving
 temp_data = []
-
 #Iterate through the Dataframe rows
-for index, row in df.iterrows():
-    num = str(row.iloc[0])  # First column: Number
-    existing_debt = row.iloc[1] # Second column: Existing debt amount
+try:
+    for index, row in df.iterrows():
+        num = str(row.iloc[0])  # First column: Number
+        existing_debt = row.iloc[1] # Second column: Existing debt amount
 
-    # Check if a debt amount already exists
-    if pd.isna(existing_debt): # Check if the value is NaN (missing)
-        debt_amount = get_debt_amount(num, API_TOKEN, logger)
+        # Check if a debt amount already exists
+        if pd.isna(existing_debt): # Check if the value is NaN (missing)
+            debt_amount = get_debt_amount(num, API_TOKEN, logger)
 
-        if debt_amount is not None:
-            df.loc[index, 'Debt Amount'] = debt_amount  # Update the DataFrame
-            temp_data.append(df.loc[[index]]) # Add the row to the temp_data list
-            logger.info(f'Found and updated debt amount for number {num} at index {index}: {debt_amount}')
-            counter += 1 # Increment a counter ONLY when a new API request is made
+            if debt_amount is not None:
+                df.loc[index, 'Debt Amount'] = debt_amount  # Update the DataFrame
+                temp_data.append(df.loc[[index]]) # Add the row to the temp_data list
+                logger.info(f'Found and updated debt amount for number {num} at index {index}: {debt_amount}')
+                counter += 1 # Increment a counter ONLY when a new API request is made
+            else:
+                df.loc[index, 'Debt Amount'] = None  # Keep it None if Error occurred
+                temp_data.append(df.loc[[index]])
+                logger.info(f'No debt found for index {index}, setting to None')
         else:
-            df.loc[index, 'Debt Amount'] = None  # Keep it None if Error occurred
-            temp_data.append(df.loc[[index]])
-            logger.info(f'No debt found for index {index}, setting to None')
-
+            logger.info(f'Debt amount already exists for number {num}. Skip API call')
         time.sleep(0.5)  # Add a small delay to avoid overwhelming the API
 
-    # Save temporary data every SAVE_INTERVAL API calls
-    if counter % SAVE_INTERVAL == 0 and counter > 0:
-        try:
-            filename = f'numbers_with_debt_temp_{counter}.xlsx'
-            # Create the full path to the temporary file
-            full_path = os.path.join(TEMP_FILES_DIR, filename)
-
-            temp_df = pd.concat(temp_data) # Create a DataFrame from the temp_data list
-            temp_df.to_excel(full_path, index=True)
-
-            logger.info(f'Data saved to {full_path} after processing {counter} API calls.')
-
-            temp_data = [] # Clear the temp_data for next save
-        except Exception as e:
-            logger.exception(f'Error saving to temporary Excel file: {e}')
-
-# Save the final result after processing all rows
-try:
-    df.to_excel('numbers_with_debt.xlsx', index=False)
-    logger.info(f'Data saved to numbers_with_debt.xlsx')
-except Exception as e:
-    logger.exception(f'Error saving to Excel file: {e}')
+        # Save temporary data every SAVE_INTERVAL API calls
+        if counter % SAVE_INTERVAL == 0 and counter > 0:
+            save_temp_data(temp_data, counter, logger, TEMP_FILES_DIR)
+            temp_data = [] # Reset temp_data after saving
+except Exception as e: # Catch any exception during processing
+    logger.exception(f'Error occurred during processing: {e}')
+finally:
+    logger.info('Saving before exiting...')
+    save_temp_data(temp_data, counter, logger, TEMP_FILES_DIR)
+    try:
+        save_dataframe_to_excel(df, FINAL_FILE, index=False, logger=logger)
+        logger.info(f'Final file saved into {FINAL_FILE}')
+    except Exception as e:
+        logger.exception(f'Error saving final Excel file: {e}')
+    logger.info('Exiting...')
+    sys.exit(0)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import pandas as pd
+
+
+def save_dataframe_to_excel(df, filename, index=False, logger=None):
+    try:
+        df.to_excel(filename, index=index)
+        if logger:
+            logger.info(f'Data saved to {filename}')
+    except Exception as e:
+        if logger:
+            logger.exception(f'Error saving to Excel file: {e}')
+        raise e
+
+
+def save_temp_data(data, counter, logger, temp_files_dir):
+    """Save data to temporary file"""
+    try:
+        filename = f'numbers_with_debt_temp_{counter}.xlsx'
+        full_path = os.path.join(temp_files_dir, filename)
+        temp_df = pd.concat(data)
+        save_dataframe_to_excel(temp_df, full_path, index=False, logger=logger)
+        logger.info(f'Data saved to {full_path} after processing {counter} API calls')
+    except Exception as e:
+        logger.exception(f"Error saving temporary file: {e}")
+
+
+def signal_handler(sig, frame, temp_data, counter, logger,temp_files_dir):
+    logger.info('Received termination signal. Saving data...')
+    save_temp_data(temp_data, counter, logger, temp_files_dir)
+    logger.info('Exiting...')
+    sys.exit(0)
